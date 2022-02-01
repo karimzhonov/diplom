9dbf1647f2e6eebe34dcf38bf37410d074865d75
Khusniddin <khtkarimzhonov@gmail.com>
2022-02-01 19:03:30 +0300

    Server Start

--- server/main/management/commands/start_socket.py
+++
@@ -1,9 +0,0 @@
-from django.core.management.base import BaseCommand
-from server.server_socket import main as socket_start
-
-
-class Command(BaseCommand):
-
-    def handle(self, *args, **options):
-        socket_start()
-
--- server_pickle/config.py
+++
@@ -1,6 +0,0 @@
-import os
-from pathlib import Path
-
-
-BASE_DIR = Path(__file__).resolve().parent
-DB_DIR = os.path.join(BASE_DIR, 'dataset')
